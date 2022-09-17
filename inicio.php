aa0e37864ccaeee15da820f5484178aa45b9c23a
Guillermo <elmemozac6@gmail.com>
2022-09-17 17:52:48 -0400

    inicio agregado 1.1

---
+++ inicio.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    <link rel="stylesheet" href="./CSS/inicio.css">
+</head>
+
+<body>
+    <nav>
+        <h1>HOTEL</h1>
+
+        <div class="container">
+            <div class="menu">
+                <a href="#" class="is-active">Inicio</a>
+                <a href="#">Sitio Web</a>
+                <a href="#">Proyecto</a>
+                <a href="#">Informacion</a>
+            </div>
+
+            <button class="hamburger">
+                <span></span>
+                <span></span>
+                <span></span>
+            </button>
+        </div>
+    </nav>
+
+    <script src="main.js"></script>
+</body>
+
+</html>
