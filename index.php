746b188d2b220475f2d2e62a3478deb34298cfbb
Guillermo <elmemozac6@gmail.com>
2022-09-26 12:44:57 -0400

    frontend del login agregado, falta backend

--- index.php
+++ index.php
@@ -1,94 +1,42 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <!----======== CSS ======== -->
-    <link rel="stylesheet" href="./CSS/style.css">
-     
-    <!----===== Iconscout CSS ===== -->
-    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
-    <link href="./IMG/logo.svg" rel="icon">
-    <title>HOTEL</title> 
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>HOTEL</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand&family=Roboto+Condensed:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./CSS/normalize.css">
+    <link rel="stylesheet" href="./CSS/login.css">
 </head>
 <body>
-    <nav>
-        <div class="logo-name">
-            <div class="logo-image">
-                <img src="./IMG/logo.svg" alt="">
-            </div>
-
-            <span class="logo_name">HOTEL</span>
-        </div>
-
-        <div class="menu-items">
-            <ul class="nav-links">
-                <li><a href="#">
-                    <i class="uil uil-calendar-alt"></i>
-                    <span class="link-name">Reservas</span>
-                </a></li>
-                <li><a href="#">
-                    <i class="uil uil-clipboard-notes"></i>
-                    <span class="link-name">Recepción</span>
-                </a></li>
-                <li><a href="#">
-                    <i class="uil uil-bed"></i>
-                    <span class="link-name">Habitación</span>
-                </a></li>
-                <li><a href="#">
-                    <i class="uil uil-file-graph"></i>
-                    <span class="link-name">Reportes</span>
-                </a></li>
-                <li><a href="#">
-                    <i class="uil uil-coffee"></i>
-                    <span class="link-name">Productos</span>
-                </a></li>
-                <li><a href="#">
-                    <i class="uil uil-setting"></i>
-                    <span class="link-name">Configuración</span>
-                </a></li>
-            </ul>
+<div class="contenedor-formulario contenedor">
+        <div class="imagen-formulario">
             
-            <ul class="logout-mode">
-                <li><a href="#">
-                    <i class="uil uil-signout"></i>
-                    <span class="link-name">Cerrar Sesión</span>
-                </a></li>
-
-               <!-- <li class="mode">
-                    <a href="#">
-                        <i class="uil uil-moon"></i>
-                    <span class="link-name">Dark Mode</span>
-                </a> 
-            -->
-                <div class="mode-toggle">
-                 <!-- <span class="switch"></span>-->
-                </div>
-            </li>
-        
-            </ul>
         </div>
-    </nav>
 
-    <section class="dashboard">
-        <div class="top">
-            <i class="uil uil-bars sidebar-toggle"></i>
-
-            <div class="search-box">
-                <i class="uil uil-search"></i>
-                <input type="text" placeholder="Search here...">
+        <form class="formulario">
+            <div class="texto-formulario">
+                <h2>HOTEL CONTROLLER</h2>
+                <p>Inicia sesión con tu cuenta</p>
             </div>
-            
-            <img src="./IMG/user.jpg" alt="">
-        </div>
-
-       
-
-        </div>
-    </section>
-
-    <script src="./JS/script.js"></script>
+            <div class="input">
+                <label for="usuario">Usuario</label>
+                <input placeholder="Ingresa tu nombre" type="text" id="usuario">
+            </div>
+            <div class="input">
+                <label for="contraseña">Contraseña</label>
+                <input placeholder="Ingresa tu contraseña" type="password" id="contraseña">
+            </div>
+            <div class="password-olvidada">
+                <a href="#">¿Olvidaste tu contraseña?</a>
+            </div>
+            <div class="input">
+                <input type="submit" value="Iniciar sesión">
+            </div>
+        </form>
+    </div>
 </body>
 </html>
