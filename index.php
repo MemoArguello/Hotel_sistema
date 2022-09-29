5635b1a1667da5ea0623f7a4b481cb250c103bc6
Guillermo Argüello Cordobe <74156373+MemoArguello@users.noreply.github.com>
2022-09-29 19:28:10 -0400

    Add files via upload
    Login agregado

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
-<meta charset="utf-8">
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>HOTEL</title>
@@ -11,32 +12,37 @@
     <link rel="stylesheet" href="./CSS/normalize.css">
     <link rel="stylesheet" href="./CSS/login.css">
 </head>
+
 <body>
-<div class="contenedor-formulario contenedor">
+
+
+    <div class="contenedor-formulario contenedor">
         <div class="imagen-formulario">
-            
+
         </div>
 
-        <form class="formulario">
+        <form action="validar.php" method="POST" class="formulario">
             <div class="texto-formulario">
                 <h2>HOTEL CONTROLLER</h2>
                 <p>Inicia sesión con tu cuenta</p>
+        
             </div>
             <div class="input">
                 <label for="usuario">Usuario</label>
-                <input placeholder="Ingresa tu nombre" type="text" id="usuario">
+                <input placeholder="Ingresa tu nombre" type="text" id="usuario" name="usuario">
             </div>
             <div class="input">
                 <label for="contraseña">Contraseña</label>
-                <input placeholder="Ingresa tu contraseña" type="password" id="contraseña">
+                <input placeholder="Ingresa tu contraseña" type="password" id="contraseña" name="contraseña">
             </div>
             <div class="password-olvidada">
                 <a href="#">¿Olvidaste tu contraseña?</a>
             </div>
             <div class="input">
-                <input type="submit" value="Iniciar sesión">
+                <input name="btningresar" type="submit" value="Iniciar sesión">
             </div>
         </form>
     </div>
 </body>
+
 </html>
