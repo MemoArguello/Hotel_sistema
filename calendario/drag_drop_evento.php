bc7c5b7bc8fc241354343778e9fb57aacac4d61b
Guillermo <elmemozac6@gmail.com>
2023-01-09 15:22:12 -0300

    cambios

--- calendario/drag_drop_evento.php
+++ calendario/drag_drop_evento.php
@@ -3,21 +3,19 @@ date_default_timezone_set("America/Bogota");
 setlocale(LC_ALL,"es_ES");
 
 include('../db.php');
-$con = conectardb();
-                        
-$idEvento= $_POST['idEvento'];
-$start= $_REQUEST['start'];
-$fecha_inicio= date('Y-m-d', strtotime($start)); 
-$end= $_REQUEST['end']; 
-$fecha_fin= date('Y-m-d', strtotime($end));  
+
+$idEvento         = $_POST['id_recepcion'];
+$start            = $_REQUEST['start'];
+$fecha_inicio     = date('Y-m-d', strtotime($start)); 
+$end              = $_REQUEST['end']; 
+$fecha_fin        = date('Y-m-d', strtotime($end));  
 
 
 $UpdateProd = ("UPDATE recepcion 
     SET 
         fecha_inicio ='$fecha_inicio',
         fecha_fin ='$fecha_fin'
-
-    WHERE id='".$idEvento."' ");
+    WHERE id_recepcion='".$idEvento."' ");
 $result = mysqli_query($con, $UpdateProd);
 
 ?>
